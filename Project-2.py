5c3e6f6a191fa8aa37b6914194bf9e7e658b2fb9
Pushkar Chavan <pushkarpchavan@gmail.com>
2024-01-12 19:01:47 -0700

    Some more methods for alpha_vantage API like monthly,intraday

--- Project-2.py
+++ Project-2.py
@@ -1,4 +1,6 @@
 from alpha_vantage.timeseries import TimeSeries
+import requests
+from bs4 import BeautifulSoup
 
 # opening text file with the API key
 with open("alpha_vantage_api.txt") as file:
@@ -10,5 +12,23 @@ print(API_KEY)
 #building the TimeSeries variable from Alpha Vantage API
 timeSeries1 = TimeSeries(key= API_KEY)
 
-#get monthly stock data of Apple
+#getting monthly stock data of Apple
 print(timeSeries1.get_monthly("AAPL"))
+
+#getting monthly stock data of Apple with string concatenation of API key
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=AAPL&apikey=" + str(API_KEY)
+r = requests.get(url)
+data = r.json()
+print(data)
+
+#getting weekly stock data of Apple and applying BeautifulSoup
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=AAPL&apikey=" + str(API_KEY)
+r = requests.get(url)
+data = BeautifulSoup(r.content)
+print(data)
+
+#getting intraday stock data of Apple
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=60min&apikey=" + str(API_KEY)
+r = requests.get(url)
+data = BeautifulSoup(r.content)
+print(data)
